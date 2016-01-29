b3b01463639956d7b30e5407f64dcd1c078f6818
kilyin <nekofaza@gmail.com>
2016-01-29 23:19:21 +0000

    Add Engine library

---
+++ Engine/Human.cpp
@@ -0,0 +1,20 @@
+// This is the main DLL file.
+
+#include "stdafx.h"
+
+#include "Human.h"
+#include <string>
+using namespace std;
+
+class Human
+{
+    Human(string name)
+    {
+
+    }
+
+    ~Human()
+    {
+
+    }
+};
---
+++ Engine/Human.h
@@ -0,0 +1,14 @@
+// Engine.h
+
+#pragma once
+
+using namespace System;
+
+namespace Engine {
+
+	public ref class Human
+	{
+		// TODO: Add your methods for this class here.
+
+	};
+}
